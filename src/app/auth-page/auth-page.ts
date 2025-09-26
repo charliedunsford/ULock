0255b5eb3209d8e66a41573534c9ff1c80e9d966
charliedunsford <184438218+charliedunsford@users.noreply.github.com>
2025-09-25 19:17:09 -0700

    feat(auth-page): add forms and partial form logic for auth-page

--- src/app/auth-page/auth-page.ts
+++ src/app/auth-page/auth-page.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Signin } from "./signin/signin";
+import { Signup } from "./signup/signup";
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-auth-page',
-  imports: [],
+  imports: [Signin, Signup, CommonModule],
   templateUrl: './auth-page.html',
   styleUrl: './auth-page.scss'
 })
 export class AuthPage {
+  formType: 'signin' | 'signup' = 'signin';
 
+  constructor(private route: ActivatedRoute) {
+    this.route.queryParams.subscribe(params => {
+      this.formType = params['form'] === 'signup' ? 'signup' : 'signin';
+    });
+  }
 }
---
+++ src/app/auth-page/signin/signin.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-signin',
+  imports: [RouterModule],
+  templateUrl: './signin.html',
+  styleUrl: './signin.scss'
+})
+export class Signin {
+
+}
